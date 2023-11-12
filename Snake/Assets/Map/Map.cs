779e3b7b643fd9e1244f1d76848947cc6eba67d7
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2023-11-12 13:04:35 +0100

    add: Map script

---
+++ Snake/Assets/Map/Map.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    SnakeGrid grid;
+    GameObject tile;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
