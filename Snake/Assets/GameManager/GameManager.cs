73a8b9fe2a8a7eb07406be76b3fe391cfa53dfa4
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2023-11-17 18:30:13 +0100

    fix: put Map functionality into GameManager script

--- Snake/Assets/GameManager/GameManager.cs
+++ Snake/Assets/GameManager/GameManager.cs
@@ -6,9 +6,29 @@ public class GameManager : MonoBehaviour {
     
     [SerializeField, Min(0)] int mapSize;
     int[,] _map;
-    
+    Map map = new Map();
+    SnakeGrid grid = new SnakeGrid();
+    [SerializeField] GameObject tile;
+
     void Awake() {
-        _map = new int[mapSize, mapSize];
+        
+    }
+
+    private void InstantiateMapTiles()
+    {
+        for (int x = 0; x < grid.height; x++)
+        {
+            for (int y = 0; y < grid.width; y++)
+            {
+                InstantiateTile(x, y);
+            }
+        }
+    }
+
+    private void InstantiateTile(int x, int y)
+    {
+        var worldPosition = new Vector3(x, y, 0);
+        Instantiate(tile, worldPosition, Quaternion.identity, this.transform);
     }
 
     void Update()
